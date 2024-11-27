eeb4b6d85d21172cc9ad2d6106e1fc77c1a71e72
nimilitao <nicolasmilivotto@gmail.com>
2024-11-27 16:40:14 -0300

    refactor: split form fields into components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,39 +1,34 @@
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import { DatePickerWithRange } from './components/ui/datepicker'
+import { FormField } from './components/formfield'
 
 import './global.css'
 
 export function App() {
   return (
-    <div className="p-6 max-w-4xl mx-auto mt-16">
+    <main className="p-6 max-w-3xl mx-auto mt-16">
       <h1 className="text-3xl font-bold mb-5">Short URL</h1>
 
       <div className="flex flex-col items-center justify-between gap-4">
         <div className="flex flex-col md:flex-row w-full gap-5">
-          <div className="flex flex-col w-full gap-2">
-            <label htmlFor="expiration" className="font-semibold">
-              Expiration time
-            </label>
-            <div className="w-full">
-              <Input placeholder="https://example.com" />
-            </div>
-          </div>
+          <FormField
+            label="Your URL"
+            inputComponent={<Input placeholder="https://example.com" />}
+            containerClassName="w-full"
+          />
 
-          <div className="flex flex-col gap-2">
-            <label htmlFor="expiration" className="font-semibold">
-              Expiration time
-            </label>
-            <div className="w-fit">
-              <DatePickerWithRange />
-            </div>
-          </div>
+          <FormField
+            label="Expiration time"
+            inputComponent={<DatePickerWithRange />}
+            containerClassName="w-fit"
+          />
         </div>
 
-        <Button className="w-full bg-primary text-primary-foreground dark:bg-gray-100 dark:text-white">
+        <Button className="w-full">
           Short URL
         </Button>
       </div>
-    </div>
+    </main>
   )
 }
