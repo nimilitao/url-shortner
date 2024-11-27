3c8746480fcca8ade9abc4ad5ef9c21e61447551
nimilitao <nicolasmilivotto@gmail.com>
2024-11-27 16:17:04 -0300

    feat: add main page components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,39 @@
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+import { DatePickerWithRange } from './components/ui/datepicker'
+
+import './global.css'
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="p-6 max-w-4xl mx-auto mt-16">
+      <h1 className="text-3xl font-bold mb-5">Short URL</h1>
+
+      <div className="flex flex-col items-center justify-between gap-4">
+        <div className="flex flex-col md:flex-row w-full gap-5">
+          <div className="flex flex-col w-full gap-2">
+            <label htmlFor="expiration" className="font-semibold">
+              Expiration time
+            </label>
+            <div className="w-full">
+              <Input placeholder="https://example.com" />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="expiration" className="font-semibold">
+              Expiration time
+            </label>
+            <div className="w-fit">
+              <DatePickerWithRange />
+            </div>
+          </div>
+        </div>
+
+        <Button className="w-full bg-primary text-primary-foreground dark:bg-gray-100 dark:text-white">
+          Short URL
+        </Button>
+      </div>
     </div>
   )
 }
