d2aea6ecb0607921bab0e8f53e38aa4f2bf8addf
nimilitao <nicolasmilivotto@gmail.com>
2024-12-14 20:23:47 -0300

    chore: update app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,59 +1,22 @@
-import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
-import { DatePickerWithRange } from './components/ui/datepicker'
-import { Toaster } from './components/ui/sonner'
-import { toast } from 'sonner'
-import { FormField } from './components/formfield'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import RedirectPage from './pages/[code]'
+import Home from './pages/home'
+import NotFoundPage from './pages/404'
 import { ThemeProvider } from './components/themeprovider'
 
-import './global.css'
-
 export function App() {
-  const handleShortenUrl = () => {
-    const shortenedUrl = 'https://exemplo.com/abc123'
-
-    const toastId = toast.success('URL successfully shortened!', {
-      duration: Infinity,
-      className: 'border-2 border-green-500 rounded-lg p-4',
-      description: `Your shortened URL: ${shortenedUrl}`,
-      action: {
-        label: 'Copy',
-
-        onClick: () => {
-          navigator.clipboard.writeText(shortenedUrl).then(() => {
-            toast.dismiss(toastId)
-            toast.success('URL copied to clipboard!')
-          })
-        },
-      },
-    })
-  }
-
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <main className="p-6 max-w-3xl mx-auto mt-16">
-        <h1 className="text-3xl font-bold mb-5">Short URL</h1>
-        <div className="flex flex-col items-center justify-between gap-4">
-          <div className="flex flex-col md:flex-row w-full gap-5">
-            <FormField
-              label="Your URL"
-              inputComponent={<Input placeholder="https://example.com" />}
-              containerClassName="w-full"
-            />
-
-            <FormField
-              label="Expiration time"
-              inputComponent={<DatePickerWithRange />}
-              containerClassName="w-fit"
-            />
-          </div>
-
-          <Button className="w-full" onClick={handleShortenUrl}>
-            Short URL
-          </Button>
-        </div>
-        <Toaster position="bottom-right" />
-      </main>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:code" element={<RedirectPage />} />
+          <Route path="*" element={<Navigate to="/notfound" replace />} />
+          <Route path="/notfound" element={<NotFoundPage />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
+
+export default App
